4587443445c8c1c00034ba434e624b50ea46c6d6
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-04-03 11:24:35 -0400

    styles with bootstrap form and POSTS form data from login info

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express'); // Bring in the express library.
 const app = express();              // Create a new express app.
-const http = require('http');
+const es6Renderer= require('express-es6-template-engine');
+
+// const http = require('http');
 const querystring = require('querystring');
+// const hostname = '127.0.0.1';
 
-const hostname = '127.0.0.1';
 const port = 3000;
-
-// Import my model class
 const Restaurant = require('./models/restaurants');
 const User = require('./models/user');
 
+app.engine('html', es6Renderer); // introduce express to es6renderer, speaks html
+app.set('views', 'views');
+app.set('view engine', 'html')// tell express to use as its view engine the thing that speaks html
+// Second argument is the directory name
+
+//Configure express to use the built in middleware 
+// that can deal with form data.
+app.use(express.urlencoded({extended:true}));
+
+
+// Import my model class
+app.get('/login', async (req , res) => {
+    // res.send('this is the login form');
+    res.render('login-form')
+});
+
+app.post('/login', async (req , res) => {
+    console.log(req.body)
+    console.log(req.body.email)
+    console.log(req.body.password)
+    res.send('yolo')
+});
 app.get('/restaurants', async (req, res) => {
     const allRestaurants = await Restaurant.getAll();    
     // const restaurantJSON = JSON.stringify(allRestaurants);    
@@ -23,6 +45,8 @@ app.get('/users', async (req, res) => {
     const allUsers = await User.getAll();
     res.json(allUsers);
 });
+
+
 app.get('/users/:id', async (req, res) => {
     // How to grab a piece out of req.params (or any object):
     // const id = req.params.id;
