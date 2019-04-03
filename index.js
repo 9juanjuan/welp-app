2840f0ae8a54aabc2932c6ab24de15a3b4a29849
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-04-03 14:10:28 -0400

    adds session file storage

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express'); // Bring in the express library.
 const app = express();              // Create a new express app.
 const es6Renderer= require('express-es6-template-engine');
+// Require my session and session storage
+//This module lets express track users as they go from page to page.
+const session = require('express-session');
+
+//Import the session storage module, and wire it up to the session module
+const FileStore = require('session-file-store')(session);
+
+// tell express to use the session modules
+app.use(session({
+    store: new FileStore(),// No options for now.
+    secret: 'sjd9j9js9djsijdinsjnajfjj'
+}));
+
+
+
+
 
 // const http = require('http');
 const querystring = require('querystring');
@@ -23,14 +39,42 @@ app.use(express.urlencoded({extended:true}));
 // Import my model class
 app.get('/login', async (req , res) => {
     // res.send('this is the login form');
-    res.render('login-form')
+    res.render('login-form', {
+        locals: {
+            email: '',
+            message: ''
+        }
+    });
 });
 
 app.post('/login', async (req , res) => {
     console.log(req.body)
     console.log(req.body.email)
     console.log(req.body.password)
-    res.send('yolo')
+    // res.send('yolo')
+    const theUser = await User.getByEmail(req.body.email);
+    console.log(theUser)
+    theUser.setPassword("SpotsSpotsSpots")
+    await theUser.save();
+    if (theUser.checkPassword(req.body.password)) {
+        //Save the users id to the session
+        req.session.user = theUser.id;
+        // Make sure the session is saved before we redirect
+        req.session.save(() => {
+            res.redirect('/dashboard')
+        })
+       console.log( 'did the if thing')
+    } else {
+        console.log('did the else thing')
+        res.render('login-form', {
+            locals: {
+                email: req.body.email,
+                message: 'Password incorrect. Please try again'
+            }
+        })
+        // send the form back, but with the email already filled out. 
+    }
+
 });
 app.get('/restaurants', async (req, res) => {
     const allRestaurants = await Restaurant.getAll();    
@@ -42,8 +86,9 @@ app.get('/restaurants', async (req, res) => {
 });
 
 app.get('/dashboard', async (req, res) => {
-    res.send('Welcome to your welp dashboard')
-})
+    console.log(`The users id is ${req.session.user}`);
+    res.send('Welcome to your welp dashboard');
+});
 
 
 app.get('/users', async (req, res) => {
